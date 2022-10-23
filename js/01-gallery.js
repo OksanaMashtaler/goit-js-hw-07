d4b7a17ae5436bc511e295904cb281232994e68d
OksanaMashtaler <mashoksana@gmail.com>
2022-10-23 11:41:21 +0300

    fix instance.close

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,31 +2,30 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.addEventListener("click", onImgClick);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
 
 function createGalleryMarkup(item) {
   return item
     .map(
       (i) =>
-        ` <div class="gallery__item">
-            <a class="gallery__link" href="${i.original}">
-              <img
-              class="gallery__image"
-              src="${i.preview}"
-              data-source="${i.original}"
-              alt="${i.description}"
-              />
-            </a>
-          </div> `
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${i.original}">
+      <img
+      class="gallery__image"
+      src="${i.preview}"
+      data-source="${i.original}"
+      alt="${i.description}"
+      />
+    </a>
+ </div> `
     )
     .join(" ");
 }
 
-const galleryMarkup = createGalleryMarkup(galleryItems);
-galleryRef.addEventListener("click", onClick);
-
-function onClick(evt) {
+function onImgClick(evt) {
   evt.preventDefault();
-
   if (evt.target.nodeName !== "IMG") {
     return;
   }
@@ -34,24 +33,50 @@ function onClick(evt) {
 }
 
 function showInstance(evt) {
+  const imgUrl = evt.target.dataset.source;
   const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}"/>`
+    `
+      <img src="${imgUrl}" width="800" height="600">
+      `,
+    {
+      onClose: () => document.removeEventListener("keydown", closeByEscape),
+    }
   );
 
   instance.show();
 
-  const closeButton = (evt) => {
-    if (evt.code !== "Escape") {
-      console.log("dd");
-      return;
-    }
-
-    if (evt.code === "Escape") {
+  function closeByEscape(evt) {
+    if (evt.key === "Escape") {
       instance.close();
-      document.removeEventListener("keydown", closeButton);
-    };
-    
-  document.addEventListener("keydown", closeButton);
+    }
+  }
+  document.addEventListener("keydown", closeByEscape);
 }
 
-galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+// function onClick(evt) {
+//   evt.preventDefault();
+
+//   if (evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   showInstance(evt);
+// }
+
+// function showInstance(evt) {
+//   const instance = basicLightbox.create(
+//     `<img src="${evt.target.dataset.source}"/>`
+//   );
+
+//   instance.show();
+
+//   const closeButton = (evt) => {
+//     if (evt.code === "Escape") {
+//       instance.close();
+//       document.removeEventListener("keydown", closeButton);
+//       console.log("j");
+//     }
+//     return;
+//   };
+
+//   document.addEventListener("keydown", closeButton);
+// }
